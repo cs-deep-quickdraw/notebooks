1fbc232d3422f3d525c8067a2a4e6502a9aa67d1
Sami Tabet <salph.tabet@gmail.com>
2020-02-26 00:09:29 +0100

    fixes

--- mesocentre/config/config_env.sh
+++ mesocentre/config/config_env.sh
@@ -1,9 +1,8 @@
 module load anaconda3/5.3.1
 module load cuda/9.0
-conda create --name quickdraw
+conda create --name quickdraw python=3.7.4
 source activate quickdraw
 cd $PBS_O_WORKDIR
-conda install quickdraw
 conda install pandas
 conda install pytorch torchvision cudatoolkit=9.2 -c pytorch
 conda install opencv
